672e06e3a554be46e289646636d7d6abed00f602
alturismo <alturismo@gmail.com>
2024-12-18 07:35:50 +0100

    update logging

--- src/usr/local/emhttp/plugins/cache-mover/scripts/cache_logs.sh
+++ src/usr/local/emhttp/plugins/cache-mover/scripts/cache_logs.sh
@@ -5,12 +5,12 @@ cache_cleaner_log="/usr/local/emhttp/plugins/cache-mover/cache_cleaner_log.txt"
 cache_mover_log="/usr/local/emhttp/plugins/cache-mover/cache_mover_log.txt"
 
 ##########################
-cat /var/log/syslog | grep -i "cache-cleaner:" >> $cache_cleaner_log
-tail -n 500 $cache_cleaner_log > myfile.tmp
+cat /var/log/syslog | grep -i "cache-cleaner:" > $cache_cleaner_log
+tail -n 999 $cache_cleaner_log > myfile.tmp
 cat myfile.tmp > $cache_cleaner_log
 rm myfile.tmp
-cat /var/log/syslog | grep -i "cache-mover:" >> $cache_mover_log
-tail -n 500 $cache_mover_log > myfile.tmp
+cat /var/log/syslog | grep -i "cache-mover:" > $cache_mover_log
+tail -n 999 $cache_mover_log > myfile.tmp
 cat myfile.tmp > $cache_mover_log
 rm myfile.tmp
 
