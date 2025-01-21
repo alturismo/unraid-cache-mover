dda8a296675422351534aee72276f2cc6ad8ea01
alturismo <alturismo@gmail.com>
2025-01-21 17:08:19 +0100

    cron mail fix

--- src/usr/local/emhttp/plugins/cache-mover/scripts/cache_cleaner_cron.sh
+++ src/usr/local/emhttp/plugins/cache-mover/scripts/cache_cleaner_cron.sh
@@ -9,7 +9,7 @@ configfile="/boot/config/plugins/cache-mover/cachemoversettings"
 ################################
 if [ ! -z "$clean_cron" ]; then
 ( crontab -l | grep -v -F "Cache_Cleaner" ; echo "# Cron Job for Cache_Cleaner plugin" ) | crontab -
-( crontab -l | grep -v -F "cache_mover3" ; echo "$clean_cron /usr/local/emhttp/plugins/cache-mover/scripts/cache_mover3" ) | crontab -
+( crontab -l | grep -v -F "cache_mover3" ; echo "$clean_cron /usr/local/emhttp/plugins/cache-mover/scripts/cache_mover3 >/dev/null 2>&1" ) | crontab -
 sed -i 's/^clean_cron_state=.*/clean_cron_state="started"/' $configfile
 else
 crontab -l | grep -v "Cache_Cleaner"  | crontab -
--- src/usr/local/emhttp/plugins/cache-mover/scripts/cache_mover_cron.sh
+++ src/usr/local/emhttp/plugins/cache-mover/scripts/cache_mover_cron.sh
@@ -9,7 +9,7 @@ configfile="/boot/config/plugins/cache-mover/cachemoversettings"
 ################################
 if [ ! -z "$script_cron" ]; then
 ( crontab -l | grep -v -F "Cache_Mover" ; echo "# Cron Job for Cache_Mover plugin" ) | crontab -
-( crontab -l | grep -v -F "cache_mover2" ; echo "$script_cron /usr/local/emhttp/plugins/cache-mover/scripts/cache_mover2" ) | crontab -
+( crontab -l | grep -v -F "cache_mover2" ; echo "$script_cron /usr/local/emhttp/plugins/cache-mover/scripts/cache_mover2 >/dev/null 2>&1" ) | crontab -
 sed -i 's/^script_cron_state=.*/script_cron_state="started"/' $configfile
 else
 crontab -l | grep -v "Cache_Mover"  | crontab -
